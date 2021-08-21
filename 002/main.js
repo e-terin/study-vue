772422d1644dfe226415e964c38be460b76304d8
Evgeniy Terin <web@derin.ru>
2021-08-21 19:10:37 +0300

    [008] усложнил немного - сделал смену картинок по кнопке

--- 002/main.js
+++ 002/main.js
@@ -2,11 +2,26 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+        images: [
+            'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+            'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+        ],
+        image: ''
     },
     methods: {
         changeImage(){
-            this.image = 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg';
+
+            // сколько у нас элементов в массиве картинок?
+            let count_images = this.images.length;
+            // текущий элемент массива
+            let current_element_index = this.images.indexOf(this.image);
+            // выставляем новый индекс
+            let new_image_index = current_element_index < (count_images - 1) ? current_element_index + 1 : 0;
+            this.image = this.images[new_image_index];
         }
+    },
+    mounted () {
+        // при загрузке устанавливаем по-умолчанию первую картинку
+        this.image = this.images[0];
     }
 })
